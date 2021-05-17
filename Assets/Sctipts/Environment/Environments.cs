f3e15f444780ee272e3cffaf9428db08f4927f1e
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-18 03:33:23 +0900

    環境変数設定用クラス ※コンストラクタで実際の値を放り込んでいるが、コンストラクタが定義されているソースコードは管理対象外

---
+++ Assets/Sctipts/Environment/Environments.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Enviroment
+{
+    /// <summary>
+    /// 環境変数
+    /// </summary>
+    public partial class Environments
+    {
+        /// <summary>
+        /// アプリケーションキー
+        /// </summary>
+        public string AppliactionKey { get; private set; }
+
+        #region Singleton
+        public static Environments Instance { get { return _Instance; } }
+        private static Environments _Instance = new Environments();
+        #endregion
+    }
+}
