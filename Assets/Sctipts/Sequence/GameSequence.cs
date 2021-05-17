23383e2f674bffc1864c43b42391a56e1d88b62a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-18 07:13:25 +0900

    動作が確認できたのでログとしてコミットして残しておく。

--- Assets/Sctipts/Sequence/GameSequence.cs
+++ Assets/Sctipts/Sequence/GameSequence.cs
@@ -19,6 +19,17 @@ namespace Game.Sequence
         void Awake()
         {
             ConnectionCore.Instance.AddCallbackTarget(this);
+            this.UpdateAsObservable()
+                .ThrottleFirst(TimeSpan.FromSeconds(3.0f))
+                .Subscribe((_) =>
+                {
+                    Dictionary<byte, object> Params = new Dictionary<byte, object>();
+                    Params[0] = 10;
+                    Params[1] = 20;
+                    Params[2] = 30;
+                    ConnectionCore.Instance.SendEvent(1, Params);
+                    Debug.Log("Send Event");
+                });
         }
 
         void OnDestroy()
@@ -28,6 +39,15 @@ namespace Game.Sequence
 
         public void OnEvent(EventData photonEvent)
         {
+            // ↓こっちは違うデータなので注意
+            // var Params = photonEvent.parameters;
+            // ↓送信した正しいデータはこっち
+            var Params = (Dictionary<byte, object>)photonEvent.CustomData;
+            for (byte i = 0; i < 3; i++)
+            {
+                var Data = (int)Params[i];
+                Debug.Log(string.Format("{0} : {1}", i, Data));
+            }
         }
     }
 }
