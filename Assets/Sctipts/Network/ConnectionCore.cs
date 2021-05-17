697a26e8f26772192301bacaae6f5b1b61227f72
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-18 03:15:05 +0900

    接続管理クラスをとりあえず定義

---
+++ Assets/Sctipts/Network/ConnectionCore.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.System;
+
+namespace Game.Network
+{
+    /// <summary>
+    /// サーバとの接続管理
+    /// </summary>
+    public class ConnectionCore : MonoBehaviour
+    {
+        /// <summary>
+        /// インスタンス
+        /// </summary>
+        /// <value></value>
+        public static ConnectionCore Instance
+        {
+            get
+            {
+                _Instance = PrefabManager.Instance.Load<ConnectionCore>("Prefabs/System/ConnectionCore");
+                return _Instance;
+            }
+        }
+        private static ConnectionCore _Instance = null;
+
+        void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
---
+++ Assets/Sctipts/System/PrefabManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.System
+{
+    /// <summary>
+    /// Prefab管理
+    /// </summary>
+    public class PrefabManager
+    {
+        /// <summary>
+        /// PrefabのDictionary
+        /// </summary>
+        private Dictionary<string, GameObject> PrefabDic = new Dictionary<string, GameObject>();
+
+        /// <summary>
+        /// 読み込み
+        /// </summary>
+        /// <param name="Path">Prefabのパス</param>
+        /// <typeparam name="T">Componentの型</typeparam>
+        /// <returns>Component</returns>
+        public T Load<T>(string Path)
+        {
+            var Prefab = FindOrLoadPrefab(Path);
+            var Obj = GameObject.Instantiate(Prefab);
+            Debug.Assert(Obj != null, "Prefab Instantiate Failed. Path:" + Path);
+
+            return Obj.GetComponent<T>();
+        }
+
+        /// <summary>
+        /// 読み込み
+        /// </summary>
+        /// <param name="Path">Prefabのパス</param>
+        /// <param name="ParentTransform">親Transform</param>
+        /// <typeparam name="T">Componentの型</typeparam>
+        /// <returns>Component</returns>
+        public T Load<T>(string Path, Transform ParentTransform)
+        {
+            var Prefab = FindOrLoadPrefab(Path);
+            var Obj = GameObject.Instantiate(Prefab, ParentTransform);
+            Debug.Assert(Obj != null, "Prefab Instantiate Failed. Path:" + Path);
+
+            return Obj.GetComponent<T>();
+        }
+
+        /// <summary>
+        /// Prefabを読み込むか保持してあるものを返す
+        /// </summary>
+        /// <param name="Path">パス</param>
+        /// <returns>Prefab</returns>
+        private GameObject FindOrLoadPrefab(string Path)
+        {
+            if (!PrefabDic.ContainsKey(Path))
+            {
+                var Prefab = Resources.Load<GameObject>(Path);
+                Debug.Assert(Prefab != null, "Prefab Load Failed. Path:" + Path);
+                PrefabDic.Add(Path, Prefab);
+            }
+            return PrefabDic[Path];
+        }
+
+        #region Singleton
+        public static PrefabManager Instance { get { return _Instance; } }
+        private static PrefabManager _Instance = new PrefabManager();
+        private PrefabManager() { }
+        #endregion
+    }
+}
