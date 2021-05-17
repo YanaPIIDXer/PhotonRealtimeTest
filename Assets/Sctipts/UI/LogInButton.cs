7ca10468f6a11ff36309a946096c9c71dae71af6
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-18 03:44:15 +0900

    ログインボタン ※機能はまだ特になし

---
+++ Assets/Sctipts/UI/LogInButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.UI
+{
+    /// <summary>
+    /// ログインボタン
+    /// </summary>
+    public class LogInButton : UIComponent
+    {
+        /// <summary>
+        /// ZOrder
+        /// </summary>
+        public override EZOrder ZOrder => EZOrder.Overlay;
+    }
+}
