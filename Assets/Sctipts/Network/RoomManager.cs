740335e46482370ae88e01a2842494cc5faf5582
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-18 06:05:37 +0900

    ルーム管理クラス定義

---
+++ Assets/Sctipts/Network/RoomManager.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using Game.System;
+using UniRx;
+using System;
+
+namespace Game.Network
+{
+    /// <summary>
+    /// ルームマネージャ
+    /// </summary>
+    public class RoomManager : MonoBehaviour, IMatchmakingCallbacks
+    {
+        /// <summary>
+        /// Prefabのパス
+        /// </summary>
+        private static readonly string PrefabPath = "Prefabs/System/RoomManager";
+
+        #region Singleton
+
+        /// <summary>
+        /// インスタンス
+        /// </summary>
+        public static RoomManager Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = PrefabManager.Instance.Load<RoomManager>(PrefabPath);
+                }
+                return _Instance;
+            }
+        }
+        private static RoomManager _Instance = null;
+
+        #endregion
+
+        /// <summary>
+        /// 参加Subject
+        /// </summary>
+        private Subject<Unit> OnJoinedSubject = new Subject<Unit>();
+
+        /// <summary>
+        /// 参加した
+        /// </summary>
+        public IObservable<Unit> OnJoined { get { return OnJoinedSubject; } }
+
+        /// <summary>
+        /// 離脱Subject
+        /// </summary>
+        private Subject<Unit> OnLeaveSubject = new Subject<Unit>();
+
+        /// <summary>
+        /// 離脱した
+        /// </summary>
+        public IObservable<Unit> OnLeave { get { return OnLeaveSubject; } }
+
+        void Awake()
+        {
+            ConnectionCore.Instance.AddCallbackTarget(this);
+            DontDestroyOnLoad(gameObject);
+        }
+
+        void OnDestroy()
+        {
+            ConnectionCore.Instance.RemoveCallbackTarget(this);
+        }
+
+        #region IMatchmakingCallbacks
+
+        public void OnCreatedRoom()
+        {
+            Debug.Log("OnCreatedRoom()");
+        }
+
+        public void OnCreateRoomFailed(short returnCode, string message)
+        {
+            Debug.LogError(string.Format("OnCreatedRoomFailed({0}, {1})", returnCode, message));
+        }
+
+        public void OnFriendListUpdate(List<FriendInfo> friendList)
+        {
+            Debug.Log("OnFriendListUpdate()  FriendCount:" + friendList.Count);
+        }
+
+        public void OnJoinedRoom()
+        {
+            OnJoinedSubject.OnNext(Unit.Default);
+        }
+
+        public void OnJoinRandomFailed(short returnCode, string message)
+        {
+            Debug.LogError(string.Format("OnJoinRandomFailed({0}, {1})", returnCode, message));
+        }
+
+        public void OnJoinRoomFailed(short returnCode, string message)
+        {
+            Debug.LogError(string.Format("OnJoinRoomFailed({0}, {1})", returnCode, message));
+        }
+
+        public void OnLeftRoom()
+        {
+            OnLeaveSubject.OnNext(Unit.Default);
+        }
+        #endregion
+    }
+}
--- Assets/Sctipts/Sequence/MatchMakeSequence.cs
+++ Assets/Sctipts/Sequence/MatchMakeSequence.cs
@@ -12,60 +12,20 @@ namespace Game.Sequence
 {
     /// <summary>
     /// マッチメイクシーケンス
-    /// TODO:多分こいつにIMatchmakingCallbacksを実装するのは違う
     /// </summary>
-    public class MatchMakeSequence : MonoBehaviour, IMatchmakingCallbacks
+    public class MatchMakeSequence : MonoBehaviour
     {
         void Awake()
         {
-            ConnectionCore.Instance.AddCallbackTarget(this);
             var Handler = UIManager.Instance.Show<MatchMakeInterface>("MatchMakeInterface");
             Handler.Instance.OnCreateRoom
                 .Subscribe((RoomName) => ConnectionCore.Instance.CreateRoom(RoomName, 2));
-        }
 
-        void OnDestroy()
-        {
-            ConnectionCore.Instance.RemoveCallbackTarget(this);
+            RoomManager.Instance.OnJoined
+                .Subscribe((_) =>
+                {
+                    SceneManager.LoadScene("Game");
+                }).AddTo(gameObject);
         }
-
-        #region IMatchmakingCallbacks
-
-        public void OnCreatedRoom()
-        {
-            Debug.Log("OnCreatedRoom()");
-        }
-
-        public void OnCreateRoomFailed(short returnCode, string message)
-        {
-            Debug.LogError(string.Format("OnCreatedRoomFailed({0}, {1})", returnCode, message));
-        }
-
-        public void OnFriendListUpdate(List<FriendInfo> friendList)
-        {
-            Debug.Log("OnFriendListUpdate()  FriendCount:" + friendList.Count);
-        }
-
-        public void OnJoinedRoom()
-        {
-            Debug.Log("OnJoinedRoom()");
-            SceneManager.LoadScene("Game");
-        }
-
-        public void OnJoinRandomFailed(short returnCode, string message)
-        {
-            Debug.LogError(string.Format("OnJoinRandomFailed({0}, {1})", returnCode, message));
-        }
-
-        public void OnJoinRoomFailed(short returnCode, string message)
-        {
-            Debug.LogError(string.Format("OnJoinRoomFailed({0}, {1})", returnCode, message));
-        }
-
-        public void OnLeftRoom()
-        {
-            Debug.Log("OnLeftRoom()");
-        }
-        #endregion
     }
 }
