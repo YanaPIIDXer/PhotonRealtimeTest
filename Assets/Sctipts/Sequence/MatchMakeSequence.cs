e6bc2270fb8e2ca3535916a3e29423628d977715
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-18 04:25:50 +0900

    マッチメイクシーケンス

---
+++ Assets/Sctipts/Sequence/MatchMakeSequence.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+
+namespace Game.Sequence
+{
+    /// <summary>
+    /// マッチメイクシーケンス
+    /// </summary>
+    public class MatchMakeSequence : MonoBehaviour
+    {
+    }
+}
