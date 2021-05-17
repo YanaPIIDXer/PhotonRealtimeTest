6b8284a69b7c782bb852eac5f438a01740d4c673
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-18 08:06:26 +0900

    ストリームクラスを持ってきた

---
+++ Assets/Sctipts/Stream/DictionaryStreamReader.cs
@@ -0,0 +1,92 @@
+using System.Collections.Generic;
+
+namespace Game.Stream
+{
+    /// <summary>
+    /// Dictionaryからデータを読み込むクラス
+    /// </summary>
+    public class DictionaryStreamReader : IDictionaryStream
+    {
+        /// <summary>
+        /// Dictionary
+        /// </summary>
+        private Dictionary<byte, object> Src = null;
+
+        /// <summary>
+        /// 現在位置
+        /// </summary>
+        private byte CurrentPosition = 0;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="Src">Dictionary</param>
+        public DictionaryStreamReader(Dictionary<byte, object> Src)
+        {
+            this.Src = Src;
+        }
+
+        public bool Serialize(ref int Data)
+        {
+            Data = (int)Src[CurrentPosition];
+            CurrentPosition++;
+            return true;
+        }
+
+        public bool Serialize(ref uint Data)
+        {
+            Data = (uint)Src[CurrentPosition];
+            CurrentPosition++;
+            return true;
+        }
+
+        public bool Serialize(ref short Data)
+        {
+            Data = (short)Src[CurrentPosition];
+            CurrentPosition++;
+            return true;
+        }
+
+        public bool Serialize(ref ushort Data)
+        {
+            Data = (ushort)Src[CurrentPosition];
+            CurrentPosition++;
+            return true;
+        }
+
+        public bool Serialize(ref char Data)
+        {
+            Data = (char)Src[CurrentPosition];
+            CurrentPosition++;
+            return true;
+        }
+
+        public bool Serialize(ref byte Data)
+        {
+            Data = (byte)Src[CurrentPosition];
+            CurrentPosition++;
+            return true;
+        }
+
+        public bool Serialize(ref float Data)
+        {
+            Data = (float)Src[CurrentPosition];
+            CurrentPosition++;
+            return true;
+        }
+
+        public bool Serialize(ref string Data)
+        {
+            Data = (string)Src[CurrentPosition];
+            CurrentPosition++;
+            return true;
+        }
+
+        public bool Serialize(ref char[] Data, int Length)
+        {
+            Data = (char[])Src[CurrentPosition];
+            CurrentPosition++;
+            return true;
+        }
+    }
+}
---
+++ Assets/Sctipts/Stream/IStream.cs
@@ -0,0 +1,77 @@
+namespace Game.Stream
+{
+    /// <summary>
+    /// ストリームインタフェース
+    /// </summary>
+    public interface IStream
+    {
+        /// <summary>
+        /// シリアライズ
+        /// </summary>
+        /// <param name="Data">データ</param>
+        /// <returns>成功したらtrueを返す</returns>
+        bool Serialize(ref int Data);
+
+        /// <summary>
+        /// シリアライズ
+        /// </summary>
+        /// <param name="Data">データ</param>
+        /// <returns>成功したらtrueを返す</returns>
+        bool Serialize(ref uint Data);
+
+        /// <summary>
+        /// シリアライズ
+        /// </summary>
+        /// <param name="Data">データ</param>
+        /// <returns>成功したらtrueを返す</returns>
+        bool Serialize(ref short Data);
+
+        /// <summary>
+        /// シリアライズ
+        /// </summary>
+        /// <param name="Data">データ</param>
+        /// <returns>成功したらtrueを返す</returns>
+        bool Serialize(ref ushort Data);
+
+        /// <summary>
+        /// シリアライズ
+        /// </summary>
+        /// <param name="Data">データ</param>
+        /// <returns>成功したらtrueを返す</returns>
+        bool Serialize(ref char Data);
+
+        /// <summary>
+        /// シリアライズ
+        /// </summary>
+        /// <param name="Data">データ</param>
+        /// <returns>成功したらtrueを返す</returns>
+        bool Serialize(ref byte Data);
+
+        /// <summary>
+        /// シリアライズ
+        /// </summary>
+        /// <param name="Data">データ</param>
+        /// <returns>成功したらtrueを返す</returns>
+        bool Serialize(ref float Data);
+
+        /// <summary>
+        /// シリアライズ
+        /// </summary>
+        /// <param name="Data">データ</param>
+        /// <returns>成功したらtrueを返す</returns>
+        bool Serialize(ref string Data);
+
+        /// <summary>
+        /// シリアライズ
+        /// </summary>
+        /// <param name="Data">データ</param>
+        /// <param name="Length">配列長</param>
+        /// <returns>成功したらtrueを返す</returns>
+        bool Serialize(ref char[] Data, int Length);
+    }
+
+    /// <summary>
+    /// Dictionaryストリームインタフェース
+    /// </summary>
+    public interface IDictionaryStream : IStream { }
+}
