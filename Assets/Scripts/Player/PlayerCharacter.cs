1a42815e55b89e6b98b959bf3c52d4b030ee725c
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-18 22:17:49 +0900

    PlayerクラスとPlayerが扱うComponentの基底クラス（MonoBehaviourではないPureなクラス）を定義

---
+++ Assets/Scripts/Player/PlayerCharacter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Player
+{
+    /// <summary>
+    /// プレイヤーキャラ
+    /// </summary>
+    public class PlayerCharacter : MonoBehaviour
+    {
+        /// <summary>
+        /// コンポーネントリスト
+        /// </summary>
+        private List<PlayerComponent> Components = new List<PlayerComponent>();
+
+        /// <summary>
+        /// PlayerComponentを登録
+        /// </summary>
+        /// <param name="Component">PlayerComponent</param>
+        private void RegisterPlayerComponent(PlayerComponent Component)
+        {
+            Components.Add(Component);
+        }
+
+        void Update()
+        {
+            foreach (var Component in Components)
+            {
+                Component.OnUpdate();
+            }
+        }
+
+        void FixedUPdate()
+        {
+            foreach (var Component in Components)
+            {
+                Component.OnFixedUpdate();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerComponent.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Player
+{
+    /// <summary>
+    /// プレイヤーコンポーネント
+    /// </summary>
+    public abstract class PlayerComponent
+    {
+        /// <summary>
+        /// 所有者
+        /// </summary>
+        private PlayerCharacter Owner = null;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="Onwer">所有者</param>
+        public PlayerComponent(PlayerCharacter Owner)
+        {
+            this.Owner = Owner;
+            Initialize();
+        }
+
+        /// <summary>
+        /// GetComponentでMonoBehaviour派生のComponentを取得
+        /// </summary>
+        /// <typeparam name="T">MonoBehaviour派生Component</typeparam>
+        /// <returns>Component</returns>
+        protected T GetMonoBehaviourComponent<T>()
+            where T : MonoBehaviour
+        {
+            return Owner.GetComponent<T>();
+        }
+
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        protected virtual void Initialize() { }
+
+        /// <summary>
+        /// Update
+        /// </summary>
+        public virtual void OnUpdate() { }
+
+        /// <summary>
+        /// FixedUpdate
+        /// </summary>
+        public virtual void OnFixedUpdate() { }
+    }
+}
