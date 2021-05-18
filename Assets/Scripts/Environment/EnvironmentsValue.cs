820463f00f119ce0caf5e15d6391248811ee4b83
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-18 11:50:06 +0900

    まさかのディレクトリ名Typo

---
+++ Assets/Scripts/Environment/EnvironmentsValue.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Enviroment
+{
+    /// <summary>
+    /// 環境変数の値
+    /// こっちのファイルはgitでは管理しない
+    /// </summary>
+    public partial class Environments
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        private Environments()
+        {
+            ApplicationKey = "1a56a985-01d7-42d5-8032-6ef6950be801";
+        }
+    }
+}
