7b91f788ba35b8063efdbe9fffefacd9d2e9b5d1
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-18 12:08:09 +0900

    とりあえず適当にパケットを定義してみる

--- Assets/Scripts/Packet/PacketID.cs
+++ Assets/Scripts/Packet/PacketID.cs
@@ -3,41 +3,9 @@
     public enum EPacketID
     {
     	/// <summary>
-		/// ログイン要求パケット
-		/// </summary>
-		LogInRequest = 0x01,
-		/// <summary>
-		/// ログイン結果パケット
-		/// </summary>
-		LogInResult = 0x02,
-		/// <summary>
-		/// クライアント準備完了パケット
-		/// </summary>
-		ClientReady = 0x03,
-		/// <summary>
-		/// サーバ準備完了パケット
-		/// </summary>
-		ServerReady = 0x04,
-		/// <summary>
-		/// プレイヤーリストパケット
-		/// </summary>
-		PlayerList = 0x05,
-		/// <summary>
 		/// プレイヤー登場パケット
 		/// </summary>
-		PlayerEnter = 0x06,
-		/// <summary>
-		/// プレイヤー移動パケット
-		/// </summary>
-		PlayerMove = 0x07,
-		/// <summary>
-		/// 他人移動パケット
-		/// </summary>
-		OtherPlayerMove = 0x08,
-		/// <summary>
-		/// プレイヤー離脱パケット
-		/// </summary>
-		PlayerLeave = 0x09,
+		PlayerEnter = 0x01,
 		
 	}
 }
---
+++ Assets/Scripts/Packet/PacketPlayerEnter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Game.Stream;
+
+namespace Game.Packet
+{
+    public class PacketPlayerEnter  : IPacket
+	{
+		
+
+		/// <summary>
+		///  ID
+		/// </summary>
+		public int Id = new int();
+
+		/// <summary>
+		///  座標
+		/// </summary>
+		public Vec3 Position = new Vec3();
+
+		
+
+		public EPacketID PacketID { get { return EPacketID.PlayerEnter; } }
+
+		public PacketPlayerEnter()
+		{
+		}
+
+		public PacketPlayerEnter(int Id, Vec3 Position)
+		{
+			this.Id = Id;
+			this.Position = Position;
+			
+		}
+
+		public bool Serialize(IStream Stream)
+		{
+			Stream.Serialize(ref Id);
+			Position.Serialize(Stream);
+			
+			return true;
+		}
+	}
+}
