449d9160cc2bbc109176147f343c886eb4f7b679
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-18 09:20:07 +0900

    タイトルシーケンス

---
+++ Assets/Sctipts/Sequence/TitleSequence.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+using Game.Network;
+using Game.UI;
+using Game.UI.Component;
+
+namespace Game.Sequence
+{
+    /// <summary>
+    /// タイトルシーケンス
+    /// </summary>
+    public class TitleSequence : MonoBehaviour
+    {
+        void Awake()
+        {
+            ConnectionEventListener.Instance.Connected
+                .Subscribe((_) =>
+                {
+                    Debug.Log("ここで次のシーンへ");
+                });
+
+            UIManager.Instance.Show<TitleScreen>("TitleScreen");
+        }
+    }
+}
