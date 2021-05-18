6231a1e5ad6cc13b1a70196bd562cb3a8fd74315
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-19 02:02:43 +0900

    正しい入退場処理の実装

--- Assets/Scripts/Sequence/GameSequence.cs
+++ Assets/Scripts/Sequence/GameSequence.cs
@@ -25,20 +25,57 @@ namespace Game.Sequence
         /// </summary>
         private Dictionary<int, PlayerCharacter> OtherPlayers = new Dictionary<int, PlayerCharacter>();
 
+        #region これどこかに定数定義無いの？
+
+        /// <summary>
+        /// プレイヤー入場イベントコード
+        /// </summary>
+        private const byte PlayerJoinCode = 255;
+
+        /// <summary>
+        /// プレイヤー退場イベントコード
+        /// </summary>
+        private const byte PlayerLeaveCode = 254;
+
+        #endregion
+
         public void OnEvent(EventData photonEvent)
         {
             Debug.Log("OnEvent Code:" + photonEvent.Code);
-            if (photonEvent.Code == 255)
-            {
-                // 何か届いてる。
-                // 実はこれが「入場」ではなかろうか？
-                // TODO:Photon Realtime側で予約しているイベントコードがあるっぽい（255も然り）ので調査
-                Debug.Log("What's the?!");
-                return;
-            }
 
+            switch (photonEvent.Code)
+            {
+                case PlayerJoinCode:
+
+                    // 入場
+                    var Player = PrefabManager.Instance.Load<PlayerCharacter>("Prefabs/System/Player");
+                    Player.SetupAsOtherPlayer();
+                    OtherPlayers.Add(photonEvent.Sender, Player);
+                    break;
+
+                case PlayerLeaveCode:
+
+                    // 退場
+                    Destroy(OtherPlayers[photonEvent.Sender].gameObject);
+                    OtherPlayers.Remove(photonEvent.Sender);
+                    break;
+
+                default:
+
+                    // その他パケット
+                    OnRecvPacket(photonEvent);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// パケットを受信した
+        /// </summary>
+        /// <param name="Data">データ</param>
+        private void OnRecvPacket(EventData Data)
+        {
             IPacket Packet = null;
-            switch ((EPacketID)photonEvent.Code)
+            switch ((EPacketID)Data.Code)
             {
                 case EPacketID.PlayerMove:
 
@@ -46,25 +83,16 @@ namespace Game.Sequence
                     break;
 
                 default:
-                    throw new Exception("Invalid Packet Code:" + photonEvent.Code);
+                    throw new Exception("Invalid Packet Code:" + Data.Code);
             }
 
-            DictionaryStreamReader Reader = new DictionaryStreamReader((Dictionary<byte, object>)photonEvent.CustomData);
+            DictionaryStreamReader Reader = new DictionaryStreamReader((Dictionary<byte, object>)Data.CustomData);
             Packet.Serialize(Reader);
 
             Vector3 Position = new Vector3();
             Position.Serialize(Reader);
 
-            if (!OtherPlayers.ContainsKey(photonEvent.Sender))
-            {
-                // 知らない人からのパケットを受信した場合、その人を生成する
-                var Player = PrefabManager.Instance.Load<PlayerCharacter>("Prefabs/System/Player");
-                Player.SetupAsOtherPlayer();
-                Player.transform.position = Position;
-                OtherPlayers.Add(photonEvent.Sender, Player);
-            }
-
-            OtherPlayers[photonEvent.Sender].OnRecvPacket(Position, Packet);
+            OtherPlayers[Data.Sender].OnRecvPacket(Position, Packet);
         }
 
         void Awake()
