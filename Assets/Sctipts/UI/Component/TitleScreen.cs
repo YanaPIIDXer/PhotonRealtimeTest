5c9f0b0a35598f822ef8dcd5f997ba726f0e08a8
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-18 09:15:58 +0900

    タイトル画面

---
+++ Assets/Sctipts/UI/Component/TitleScreen.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using System;
+using Game.Network;
+
+namespace Game.UI.Component
+{
+    /// <summary>
+    /// タイトル画面
+    /// </summary>
+    public class TitleScreen : UIComponent
+    {
+        /// <summary>
+        /// ZOrder
+        /// </summary>
+        public override EZOrder ZOrder => EZOrder.MainHUD;
+
+        /// <summary>
+        /// スタートボタン
+        /// </summary>
+        [SerializeField]
+        private Button StartButton = null;
+
+        void Awake()
+        {
+            StartButton.OnClickAsObservable()
+                .Subscribe((_) =>
+                {
+                    StartButton.interactable = false;
+                    ServerConnection.Instance.Connect();
+                });
+        }
+    }
+}
