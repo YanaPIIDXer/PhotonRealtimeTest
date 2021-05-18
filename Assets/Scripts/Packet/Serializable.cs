cdc6a75f7b2ce7915c8cad516f6733ec6e280675
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-18 12:03:03 +0900

    ISerializableとFlexArrayを持ってきた

---
+++ Assets/Scripts/Packet/Serializable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game.Stream;
+
+namespace Game.Packet
+{
+    /// <summary>
+    /// シリアライズ可能オブジェクトインタフェース
+    /// </summary>
+    public interface ISerializable
+    {
+        /// <summary>
+        /// シリアライズ
+        /// </summary>
+        /// <param name="Stream">ストリーム</param>
+        /// <returns>成功したらtrueを返す</returns>
+        bool Serialize(IStream Stream);
+    }
+}
